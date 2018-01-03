905dc8ac23ec04007178fce9c4553a5bfec40a7c
dermatty <stephan@untergrabner.at>
2018-01-03 22:17:14 +0100

    rewrite, nest 1

--- bin/zenz/zenz2.py
+++ bin/zenz/zenz2.py
@@ -16,7 +16,21 @@ from paramiko import SSHClient
 import guckmongo
 import zenzlib
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import signal
+import sys
+import sseclient   # Achtung: sseclient-py !!!
+import urllib3
+import http
+from urllib.parse import urlparse
+import certifi
+from threading import Thread
 
+updater = None
+
+# NEST
+urllib3.disable_warnings()
+NEST_API_URL = "https://developer-api.nest.com"
+TOKEN = "c.SMjBF19NuZ0Bm7ySJFf0nqyoMohWO2aXOL6u2Kjjx7REcylX3kiLRiYQrQlsP06cNvNf4QEJMiIcGZYkjcd8xlDXsajpwxWKUDgyFvniTefFPXcK8kKFA2ztde1BBGACcoa1dhhHIPcfAane"
 
 # try to get config & DB
 try:
@@ -42,11 +56,21 @@ if _guck_home[-1] != "/":
 # replace for ubuntuserver
 #guck_home = _guck_home.replace("/nfs/NFS_Projekte/", "/nfs_neu/")
 # for ubuntuvm1
-guck_home = _guck_home.replace("/nfs/NFS_Projekte/", "/nfs/")
+
+#!!! guck_home = _guck_home.replace("/nfs/NFS_Projekte/", "/nfs/")
+guck_home = _guck_home
+
 #guck_home = _guck_home
 #print(guck_home)
 os.environ["GUCK_HOME"] = guck_home
 GUCK_HOME = os.environ["GUCK_HOME"]
+
+
+def sighandler(signum, frame):
+    global updater
+    updater.stop()
+    # nest stop
+    sys.exit()
 
 
 def send_to_guck(bot, update):
@@ -153,6 +177,85 @@ def sendtext_toGuck(msg, bot, update, host, port):
         return False
 
 
+class Nest_sse(Thread):
+    def __init__(self, token, api_endpoint):
+        Thread.__init__(self)
+        self.daemon = True
+        self.TOKEN = token
+        self.API_ENDPOINT = api_endpoint
+        self.STATUS = 1
+        self.MSG = ""
+
+    def run(self):
+        headers0 = {
+            'Authorization': "Bearer " + self.TOKEN,
+            'Accept': 'text/event-stream'
+        }
+        url = self.API_ENDPOINT
+
+        # Test for redirect
+        conn = http.client.HTTPSConnection("developer-api.nest.com")
+        headers = {'authorization': "Bearer {0}".format(self.TOKEN)}
+        conn.request("GET", "/", headers=headers)
+        response = conn.getresponse()
+        if response.status == 307:
+            redirectLocation = urlparse(response.getheader("location"))
+            conn = http.client.HTTPSConnection(redirectLocation.netloc)
+            conn.request("GET", "/", headers=headers)
+            response = conn.getresponse()
+            if response.status != 200:
+                self.STATUS = -2
+                self.MSG = "Cannot connect, redirect with non 200 response, aborting ..."
+                return
+            url = "https://" + redirectLocation.netloc
+
+        url += "/structures/f1iJ7Qyvwlr6dvj2MMhHl9VVSqYQ11gMpl1bfDYOESORRo7IkewcYw/away"
+        # url += "/devices/smoke_co_alarms/OLK-rC0gUkeZ9nH3no2m-iMePXBie9RS/last_connection"
+        http0 = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+        response = http0.request('GET', url, headers=headers0, preload_content=False)
+        print(url)
+
+        if response.status != 200:
+            self.STATUS = -2,
+            self.MSG = "Cannot connect to redirect, aborting ..."
+            return
+        try:
+            client = sseclient.SSEClient(response)
+        except Exception as e:
+            self.STATUS = -2,
+            self.MSG = str(e)
+
+        self.STATUS = 1
+        print("Waiting for event ...")
+
+        for event in client.events():  # returns a generator
+            try:
+                event_type = event.event
+                print("event: ", event_type)
+                if event_type == 'open':  # not always received here
+                    print("The event stream has been opened")
+                elif event_type == 'put':
+                    print("The data has changed (or initial data sent)")
+                    print("data: ", event.data)
+                elif event_type == 'keep-alive':
+                    print("No data updates. Receiving an HTTP header to keep the connection open.")
+                elif event_type == 'auth_revoked':
+                    print("The API authorization has been revoked.")
+                    print("revoked token: ", event.data)
+                    raise Exception("AUTH ERROR")
+                elif event_type == 'error':
+                    print("Error occurred, such as connection closed.")
+                    print("error message: ", event.data)
+                    raise Exception(str(event.data))
+                else:
+                    print("Unknown event, no handler for it.")
+            except Exception as e:
+                self.STATUS = -1
+                if str(e) == "AUTH_ERROR":
+                    self.STATUS = -2
+                self.MSG = str(e)
+
+
 if __name__ == '__main__':
     ln = "zenz"
     logger = logging.getLogger(ln)
@@ -182,18 +285,14 @@ if __name__ == '__main__':
 
     # Create the EventHandler and pass it your bot's token.
     updater = Updater(TOKEN)
-
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    # dp.add_handler(CommandHandler("g.ping", g_ping))
-    # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, chandler))
 
-    # log all errors
-    # dp.add_error_handler(error)
+    signal.signal(signal.SIGINT, sighandler)
+    signal.signal(signal.SIGTERM, sighandler)
+    signal.signal(signal.SIGABRT, sighandler)
 
     updater.start_polling()
 
-    updater.idle()
+    while True:
+        time.sleep(0.5)
